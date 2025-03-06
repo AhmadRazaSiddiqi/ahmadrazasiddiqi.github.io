219e8662a223ce632f5c17f33de8c99c1b4d39fe
AhmadRazaSiddiqi <ahmadrazasiddiqi5@gmail.com>
2025-03-06 22:40:57 +0500

    Made Responsive And added menu

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,13 +1,19 @@
 const MENU=document.querySelector('.menu-div')
 const menu = document.querySelector(".fa-bars")
+const inner = document.querySelector(".menu-inner")
 const close = document.querySelector(".fa-xmark")
-MENU.style.display='none'
-menu.addEventListener("click", ()=>{
-MENU.style.display='block'
-menu.style.display='none';
-close.style.display='block';
+
+menu.addEventListener('click',()=>{
+    menu.classList.add('hidden')
+    close.classList.remove('hidden')
+    inner.classList.remove('hidden')
+    inner.classList.add('flex')
+    inner.classList.add('flex-col')
+
+    
 })
-close.addEventListener("click", ()=>{
-    close.style.display='none';
-    menu.style.display='block';
-    })
+close.addEventListener('click',()=>{
+    close.classList.add('hidden')
+    inner.classList.add('hidden')
+    menu.classList.remove('hidden')
+})
