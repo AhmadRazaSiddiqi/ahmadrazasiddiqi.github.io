1fe78faff898a6d0e22aa12625f5245c0533da19
AhmadRazaSiddiqi <ahmadrazasiddiqi5@gmail.com>
2025-03-07 14:28:09 +0500

    Updated CV

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,19 +1,17 @@
-const MENU=document.querySelector('.menu-div')
+const MENU = document.querySelector(".menu-div")
 const menu = document.querySelector(".fa-bars")
 const inner = document.querySelector(".menu-inner")
 const close = document.querySelector(".fa-xmark")
 
-menu.addEventListener('click',()=>{
-    menu.classList.add('hidden')
-    close.classList.remove('hidden')
-    inner.classList.remove('hidden')
-    inner.classList.add('flex')
-    inner.classList.add('flex-col')
-
-    
+menu.addEventListener("click", () => {
+  menu.classList.add("hidden")
+  close.classList.remove("hidden")
+  inner.classList.remove("hidden")
+  inner.classList.add("flex")
+  inner.classList.add("flex-col")
 })
-close.addEventListener('click',()=>{
-    close.classList.add('hidden')
-    inner.classList.add('hidden')
-    menu.classList.remove('hidden')
+close.addEventListener("click", () => {
+  close.classList.add("hidden")
+  inner.classList.add("hidden")
+  menu.classList.remove("hidden")
 })
